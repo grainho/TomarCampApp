02fb1282c102dd6b253c2ed0376efaf9549fe507
gonçalo <gonalo@DESKTOP-5RVGG75>
2019-07-02 11:26:29 +0100

    Criação do mecanismo de associação da criança ao pai

---
+++ TomarCampApp/TomarCampApp/Controllers/CriancasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TomarCampApp.Models;
+
+namespace TomarCampApp.Controllers
+{
+    public class CriancasController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Criancas
+        public ActionResult Index()
+        {
+            var criancas = db.Criancas.Include(c => c.Pai);
+            return View(criancas.ToList());
+        }
+
+        // GET: Criancas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Criancas criancas = db.Criancas.Find(id);
+            if (criancas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(criancas);
+        }
+
+        // GET: Criancas/Create/5
+        public ActionResult Create(int ? id)
+        {
+            ViewBag.PaiFK = new SelectList(db.Pais, "ID", "Nome", id);
+            return View();
+        }
+
+        // POST: Criancas/Create/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nome,Idade,Doencas,NumCC,NIF,PaiFK")] Criancas criancas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Criancas.Add(criancas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PaiFK = new SelectList(db.Pais, "ID", "Nome", criancas.PaiFK);
+            return View(criancas);
+        }
+
+        // GET: Criancas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Criancas criancas = db.Criancas.Find(id);
+            if (criancas == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PaiFK = new SelectList(db.Pais, "ID", "Nome", criancas.PaiFK);
+            return View(criancas);
+        }
+
+        // POST: Criancas/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nome,Idade,Doencas,NumCC,NIF,PaiFK")] Criancas criancas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(criancas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PaiFK = new SelectList(db.Pais, "ID", "Nome", criancas.PaiFK);
+            return View(criancas);
+        }
+
+        // GET: Criancas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Criancas criancas = db.Criancas.Find(id);
+            if (criancas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(criancas);
+        }
+
+        // POST: Criancas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Criancas criancas = db.Criancas.Find(id);
+            db.Criancas.Remove(criancas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- TomarCampApp/TomarCampApp/Controllers/PaisController.cs
+++ TomarCampApp/TomarCampApp/Controllers/PaisController.cs
@@ -10,6 +10,7 @@ using TomarCampApp.Models;
 
 namespace TomarCampApp.Controllers
 {
+    [Authorize]
     public class PaisController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
@@ -25,50 +26,33 @@ namespace TomarCampApp.Controllers
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                //este erro ocorre porque o utilizador anda a fazer asneiras
+                return RedirectToAction("Index");
             }
             Pais pais = db.Pais.Find(id);
             if (pais == null)
             {
-                return HttpNotFound();
-            }
-            return View(pais);
-        }
-
-        // GET: Pais/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Pais/Create
-        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
-        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Nome,Idade,NumCC,NIF,Telemovel,Email")] Pais pais)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Pais.Add(pais);
-                db.SaveChanges();
+                //O Funcionario não foi encontrado, porque o utilizador está a fazer asneira
                 return RedirectToAction("Index");
             }
-
             return View(pais);
         }
+
+        
 
         // GET: Pais/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                //este erro ocorre porque o utilizador anda a fazer asneiras
+                return RedirectToAction("Index");
             }
             Pais pais = db.Pais.Find(id);
             if (pais == null)
             {
-                return HttpNotFound();
+                //o Funcionario nao foi encontrado porque o utilizador anda a fazer asneiras
+                return RedirectToAction("Index");
             }
             return View(pais);
         }
@@ -90,28 +74,61 @@ namespace TomarCampApp.Controllers
         }
 
         // GET: Pais/Delete/5
+        [Authorize(Roles = "Func")]
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                //este erro ocorre porque o utilizador anda a fazer asneiras
+                return RedirectToAction("Index");
             }
             Pais pais = db.Pais.Find(id);
             if (pais == null)
             {
-                return HttpNotFound();
+                //o Funcionario nao foi encontrado porque o utilizador anda a fazer asneiras
+                return RedirectToAction("Index");
             }
+            Session["pai"] = pais.ID;
             return View(pais);
         }
 
         // POST: Pais/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Func")]
         public ActionResult DeleteConfirmed(int id)
         {
+            if (id == null)
+            {
+
+                //o utilizar está a tentar dar-me a volta ao código
+                return RedirectToAction("Index");
+            }
+            //o ID não é null
+            //será o ID o que eu espero?
+            // vamos validar se o ID está correto
+            if (id != (int)Session["pai"])
+            {
+                // Caso o utilizador esteja a fazer asneiras
+                return RedirectToAction("Index");
+            }
+
             Pais pais = db.Pais.Find(id);
-            db.Pais.Remove(pais);
-            db.SaveChanges();
+            if (pais == null)
+            {
+                //nao foi encontrado o agente
+                return RedirectToAction("Index");
+            }
+            try
+            {
+                db.Pais.Remove(pais);
+                db.SaveChanges();
+            }catch (Exception)
+            {
+                //informar que houve um erro
+                ModelState.AddModelError("", "Não é possivel remover o Encarregado de Educação " + pais.Nome + ". Provalvemente possui concretizações associadas a ele");
+                return View(pais);
+            }
             return RedirectToAction("Index");
         }
 
